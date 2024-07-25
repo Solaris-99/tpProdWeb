b0df7676d892c3cd63cb0f7df2bbc61dff3771c2
Solaris-99 <e.cifuentes996@gmail.com>
2024-07-25 17:58:45 -0300

    created 403 error page

---
+++ public/front/403.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<?php include_once __DIR__.'/./partials/head.php' ?>
+
+<body>
+    <?php include_once __DIR__.'/./partials/nav.php' ?>
+    <main>
+        <div class="d-flex flex-row align-items-center" style="min-height:100vh">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <span class="display-1 d-block">403</span>
+                    <div class="mb-4 lead">No tienes permiso para ver este recurso.</div>
+                    <a href="index.php" class="btn btn-link">Volver al inicio</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </main>
+    <?php include_once __DIR__.'/./partials/footer.php' ?>
+</body>
+
+</html>
